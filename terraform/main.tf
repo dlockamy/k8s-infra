749fbe947ad6310a2472e04fc20b3e043c536c9a
Douglas Lockamy <douglaslockamy@MacBook-Pro.local>
2026-02-14 22:16:37 -0500

    debugging

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,6 @@ terraform {
       source  = "hashicorp/local"
       version = "~> 2.0"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 2.0"
-    }
     vault = {
       source  = "hashicorp/vault"
       version = "~> 3.0"
